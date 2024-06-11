5a3980fc5d2c2c1b8b47632ecb1ac59f3b6633ba
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-11 17:34:03 -0500

    Agregando informacion al dashboard

--- Frontend/src/routes/Routers.jsx
+++ Frontend/src/routes/Routers.jsx
@@ -6,8 +6,12 @@ import Signup from '../pages/Singup';
 import Contact from '../pages/Contact';
 import Doctors from '../pages/Doctors/Doctors';
 import DoctorDetails from '../pages/Doctors/DoctorsDetails';
+import Myaccount from '../Dashboard/user-account/MyAccount';
+import Dashboard from '../Dashboard/doctor-account/Dashboard';
+
 
 import {Routes, Route} from 'react-router-dom'
+import ProtectedRoute from './ProtectedRoute';
 
 
 const Routers = () => {
@@ -20,6 +24,22 @@ const Routers = () => {
         <Route path="/Register" element={<Signup/>}/>
         <Route path="/Contact" element={<Contact/>}/>
         <Route path="/Services" element={<Services/>}/>
+        <Route 
+            path="/users/profile/me" 
+            element={
+                <ProtectedRoute allowedRoles={['patient']}>
+                    <Myaccount/>
+                </ProtectedRoute>                    
+            }
+        />
+        <Route 
+            path="/doctors/profile/me" 
+            element={
+                <ProtectedRoute allowedRoles={['doctor']}>                    
+                    <Dashboard/>
+                </ProtectedRoute>    
+            }
+        />
     </Routes>  
 };
 
