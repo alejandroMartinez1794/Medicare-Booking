2236408290ff10d17011231c2e4bd5d898c1284e
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-12 15:10:34 -0500

    My Account changes

--- Frontend/src/Dashboard/user-account/MyAccount.jsx
+++ Frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import {useContext, useState} from 'react';
 import userImg from '../../assets/images/doctor-img01.png';
+import {authContext} from '../../context/AuthContext';
 
-const Myaccount = () => {
+import MyBookings from './MyBookings';
+import Profile from './Profile';
+
+
+const MyAccount = () => {
+
+    const {dispatch} = useContext(authContext);
+    const [tab, setTab] = useState('bookings');    
+
+    const handleLogout = () => {
+        dispatch({type: 'LOGOUT'});
+    };
+
     return (
-        <div className='max-w-[1170px] px-5 mx-auto'>
+        <section>
+            <div className='max-w-[1170px] px-5 mx-auto'>
             <div className='grid md:grid-cols-3 gap-10'>
                 <div className='pb-[50px] px-[30px] rounded-md'>
                     <div className='flex items-center justify-center'>
@@ -32,19 +46,49 @@ const Myaccount = () => {
                     </div>    
 
                     <div className='mt-[50px] md:mt-[100px]'>
-                        <button className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>
+                        <button 
+                            onClick={handleLogout}
+                            className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'
+                        >
                             Logout
                         </button>
                         <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>
                             Delete Account
                         </button>     
                     </div>    
-
-
                 </div>
+
+                <div className='md:col-span-2 md:px-[30px]'>
+                    <div>
+                        <button
+                            onClick={() => setTab('bookings')}
+                            className={` ${ 
+                                tab === "bookings" && "bg-primaryColor text-white font-normal"   
+                            }   p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] 
+                            leading-7 border border-solid border-primaryColor`}	
+                        >
+                            My Bookings
+                        </button>
+
+                        <button
+                            onClick={() => setTab('settings')}
+                            className={` ${ 
+                                tab === "settings" && "bg-primaryColor text-white font-normal"
+                            }   py-2 px-5 rounded-md text-headingColor font-semibold text-[16px]
+                            leading-7 border border-solid border-primaryColor`}
+                        >
+                            Profile Settings
+                        </button>
+                    </div>
+
+                    {tab === 'bookings' && <MyBookings/>}
+                    {tab === 'settings' && <Profile/>}
+                </div>
+
             </div>
-        </div>
+            </div>
+        </section>
     );
 };
 
-export default Myaccount;    
+export default MyAccount;    
