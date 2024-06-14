c4a6658c1cfaa95069f3123321cad66695679733
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-14 14:58:58 -0500

    update useFetchData hook to include token from config

--- Frontend/src/Dashboard/user-account/MyAccount.jsx
+++ Frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -8,6 +8,8 @@ import Profile from './Profile';
 import useGetProfile from '../../hooks/useFetchData';
 import { BASE_URL } from '../../config';
 
+import Loading from "../../components/Loader/Loading";
+import Error from "../../components/Error/Error";
 
 const MyAccount = () => {
 
@@ -29,74 +31,79 @@ const MyAccount = () => {
     return (
         <section>
             <div className='max-w-[1170px] px-5 mx-auto'>
-            <div className='grid md:grid-cols-3 gap-10'>
-                <div className='pb-[50px] px-[30px] rounded-md'>
-                    <div className='flex items-center justify-center'>
-                        <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
-                            <img
-                                src={userImg}
-                                alt=''
-                                className='w-full h-full rounded-full'
-                            />
-                        </figure>    
+                
+                {loading && !error && <Loading/>}
+
+                { error && !loading && <Error errMessage={error}/>}
+
+                {!loading && !error && 
+                    <div className='grid md:grid-cols-3 gap-10'>
+                        <div className='pb-[50px] px-[30px] rounded-md'>
+                            <div className='flex items-center justify-center'>
+                                <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
+                                    <img
+                                        src={userImg}
+                                        alt=''
+                                        className='w-full h-full rounded-full'
+                                    />
+                                </figure>    
+                            </div>
+
+                            <div className='text-center mt-4'>
+                                <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'> 
+                                    Alejandro Martinez
+                                </h3>
+                                <p className='text-textColor text-[15px] leading-6 font-medium'>
+                                    Siento que el exito me llama. 420, lo mas fresa del pedazo.
+                                </p>
+                                <p className='text-textColor text-[15px] leading-6 font-medium'>
+                                    Blood Type
+                                    <span className='ml-2 text-headingColor text-[22px] leading-8'>
+                                        O+
+                                    </span>
+                                </p>
+                            </div>    
+
+                            <div className='mt-[50px] md:mt-[100px]'>
+                                <button 
+                                    onClick={handleLogout}
+                                    className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'
+                                >
+                                    Logout
+                                </button>
+                                <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>
+                                    Delete Account
+                                </button>     
+                            </div>    
+                        </div>
+
+                        <div className='md:col-span-2 md:px-[30px]'>
+                            <div>
+                                <button
+                                    onClick={() => setTab('bookings')}
+                                    className={` ${ 
+                                        tab === "bookings" && "bg-primaryColor text-white font-normal"   
+                                    }   p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] 
+                                    leading-7 border border-solid border-primaryColor`}	
+                                >
+                                    My Bookings
+                                </button>
+
+                                <button
+                                    onClick={() => setTab('settings')}
+                                    className={` ${ 
+                                        tab === "settings" && "bg-primaryColor text-white font-normal"
+                                    }   py-2 px-5 rounded-md text-headingColor font-semibold text-[16px]
+                                    leading-7 border border-solid border-primaryColor`}
+                                >
+                                    Profile Settings
+                                </button>
+                            </div>
+                            {tab === 'bookings' && <MyBookings/>}
+                            {tab === 'settings' && <Profile/>}
+                        </div>        
                     </div>
-
-                    <div className='text-center mt-4'>
-                        <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'> 
-                            Alejandro Martinez
-                        </h3>
-                        <p className='text-textColor text-[15px] leading-6 font-medium'>
-                            Siento que el exito me llama. 420, lo mas fresa del pedazo.
-                        </p>
-                        <p className='text-textColor text-[15px] leading-6 font-medium'>
-                            Blood Type
-                            <span className='ml-2 text-headingColor text-[22px] leading-8'>
-                                O+
-                            </span>
-                        </p>
-                    </div>    
-
-                    <div className='mt-[50px] md:mt-[100px]'>
-                        <button 
-                            onClick={handleLogout}
-                            className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'
-                        >
-                            Logout
-                        </button>
-                        <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>
-                            Delete Account
-                        </button>     
-                    </div>    
-                </div>
-
-                <div className='md:col-span-2 md:px-[30px]'>
-                    <div>
-                        <button
-                            onClick={() => setTab('bookings')}
-                            className={` ${ 
-                                tab === "bookings" && "bg-primaryColor text-white font-normal"   
-                            }   p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] 
-                            leading-7 border border-solid border-primaryColor`}	
-                        >
-                            My Bookings
-                        </button>
-
-                        <button
-                            onClick={() => setTab('settings')}
-                            className={` ${ 
-                                tab === "settings" && "bg-primaryColor text-white font-normal"
-                            }   py-2 px-5 rounded-md text-headingColor font-semibold text-[16px]
-                            leading-7 border border-solid border-primaryColor`}
-                        >
-                            Profile Settings
-                        </button>
-                    </div>
-
-                    {tab === 'bookings' && <MyBookings/>}
-                    {tab === 'settings' && <Profile/>}
-                </div>
-
-            </div>
+                }
             </div>
         </section>
     );
