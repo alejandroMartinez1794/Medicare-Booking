c5ef767d141659746b052a0381320687fe07855e
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-15 21:20:33 -0500

    Refactor Dashboard component to include loading and error handling

--- Frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ Frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import Loader from '../../components/Loader/Loader';
+import Error from '../../components/Error/Error';
+import { useGetPRofile } from "../../hooks/useFetchData";
+import { BASE_URL } from "../../config";
+
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
+
+    const { data, loading, error } = useGetPRofile(
+        `${BASE_URL}/doctors/profile/me`
     );
+
+    return (
+        <section>
+            <div className='max-w-[1170px] px-5 mx-auto'>
+                {loading && !error && <Loader />}
+                {error && !loading && <Error />}
+            
+                {!loading && !error && data && (
+                    <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
+                    
+                    </div>
+                )};
+            </div>
+        </section>
+    )
 };
+
+
 
 export default Dashboard;
