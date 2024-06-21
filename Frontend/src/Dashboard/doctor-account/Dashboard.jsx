add8ce7d50bf98d3e81e813bb5f31eb44936a0f0
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-20 22:28:37 -0500

    Refactor doctor account tabs component and add pending approval notification

--- Frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ Frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -23,8 +23,35 @@ const Dashboard = () => {
                 {!loading && !error && data && (
                     <div className='grid lg:grid-cols-3 gap-[30px] lg:gap-[50px]'>
                         <Tabs tab={tab} setTab={setTab}/>
+                        <div className='lg:col-span-2'>
+                            {data.isApproved === 'pending' && (
+                                <div className='flex p-4 mb-4 text-yellow-800 bg-yellow-50 rounded-lg'>
+                                    <svg
+                                        aria-hidden="true"
+                                        className="flex-shrink-0 w-5 h-5"
+                                        fill="currentColor"
+                                        viewBox= "0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            fillRule= "evenodd"
+                                            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 
+                                            0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                                            clipRule= "evenodd"
+                                        ></path>
+                                    </svg>
+
+                                    <span className='sr-only'>Info</span>
+                                    <div className='ml-3 text-sm font-medium'>
+                                        To get approved, please complete your profile. we&apos;ll 
+                                        review manually and approve within 3days.
+                                    </div>
+
+                                </div>
+                            )}
+                        </div>
                     </div>
-                )};
+                )}
             </div>
         </section>
     )
