81ab607145787d34ea67f345f8457a58abec2c9e
Luis Alejandro Martinez Florez <lamartinezf@libertadores.edu.co>
2024-06-21 23:04:04 -0500

    Refactor doctor account tabs component and add lazy loading for images

--- Frontend/src/Dashboard/doctor-account/Dashboard.jsx
+++ Frontend/src/Dashboard/doctor-account/Dashboard.jsx
@@ -4,7 +4,7 @@ import Error from '../../components/Error/Error';
 import useGetProfile  from "../../hooks/useFetchData";
 import { BASE_URL } from "../../config";
 import Tabs from './Tabs';
-
+import starIcon from '../../assets/images/Star.png';
 
 const Dashboard = () => {
 
@@ -44,11 +44,51 @@ const Dashboard = () => {
                                     <span className='sr-only'>Info</span>
                                     <div className='ml-3 text-sm font-medium'>
                                         To get approved, please complete your profile. we&apos;ll 
-                                        review manually and approve within 3days.
+                                        review manually and approve within 3 days.
                                     </div>
 
                                 </div>
                             )}
+
+                            <div className='mt-8'>
+                                {tab === 'overview' && (
+                                    <div>
+                                        <div className='flex items-center gap-4 mb-10'>
+                                            <figure className='max-w-[200px] max-h-[200px]'>
+                                                <img src={data?.photo} alt="" className='w-full'/> 
+                                            </figure>
+                                            <div>
+                                                <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-4 lg:py-2 lg:px-6 rounded text-[12px]
+                                                leading-4 lg:text-[16px] lg:leading-6 font-semibold'>
+                                                    Surgeon
+                                                </span>
+
+                                                <h3 className='text-[22px] leading-9 font-bold text-headingColor mt-3'>
+                                                    Alejandro Martinez
+                                                </h3>
+                                                <div className='flex items-center gap-[6px]'>
+                                                    <span className='flex items-center gap-[6px] text-headingColor text-[14px] leading-5
+                                                    lg:text-[16px] lg:leading-6 font-semibold'>
+                                                        <img src={starIcon} alt=""/>
+                                                        4.5
+                                                    </span> 
+                                                    <span className='text-textColor text-[14px] leading-5 lg:text-[16px] lg:leading-6 
+                                                    font-semibold'>
+                                                        (233)
+                                                    </span> 
+                                                </div>
+                                                <p className='text_para font-[15px] lg:max-w-[390px] leading-6'>
+                                                    Doctor Bio
+                                                </p>
+                                            </div>                                    
+                                        </div>
+                                    </div>
+                                )}
+
+                                {tab === 'appointments' && <div>appointments</div>}
+                                {tab === 'settings' && <div>profile settings</div>}
+                            </div>
+
                         </div>
                     </div>
                 )}
